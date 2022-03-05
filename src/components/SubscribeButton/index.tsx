0ab3113771f54c2921e9ce3e95ac7a6a5bd1fe2b
Sergio Damaceno Botelho <sergio.damaceno0002@gmail.com>
2022-03-05 03:54:23 -0300

    Gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,30 @@
+import { useSession, signIn} from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+//getServerSideProps (SSR)
+//getStaticProps(SSG)
+// API Routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession()
+    
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+    }
+    
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button 
+        type="button" 
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     )
