bfb94663ad813e4e6180a1ee73d4b2467371f74b
Sergio Damaceno Botelho <sergio.damaceno0002@gmail.com>
2022-03-11 22:54:26 -0300

    Configurando Prismic CMS, CONSUMINDO API DO PRISMIC, LISTANDO POSTS EM TELA, PAGIN POST, VALIDANDO ASSINATURA ATIVA, PAGINA PREVIEW DO POST

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn} from 'next-auth/react';
+import Router, { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -13,10 +14,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession()
+    const router = useRouter()
     
     async function handleSubscribe() {
         if (!session) {
             signIn('github');
+            return;
+        }
+
+        if (session.activeSubscription) {
+            router.push('/posts');
             return;
         }
 
